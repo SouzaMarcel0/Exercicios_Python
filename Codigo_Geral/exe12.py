942df0f720c638c45c19a0d442eb2120b81fe1b3
MarceloSouza <souza.marcelogi@gmail.com>
2021-09-26 18:38:38 -0300

    whilepy

---
+++ Codigo_Geral/exe12.py
@@ -0,0 +1,19 @@
+
+
+print ('**' * 18)
+print ('**** Bem Vindo ao Meu Programa ****\n')
+
+resposta = input('Bora dá um rolê [s/n]??   ')
+n = 1
+
+while resposta != 's':
+    resposta = input(f'Bora{"a" * n} [s/n]??   ')
+    n += 1
+    if 'chato' in resposta or 'chata' in resposta:
+        print('Foi mal!')
+        break
+else:
+    print(f'Então, bora{"a" * n}!!!!')
+
+
+
