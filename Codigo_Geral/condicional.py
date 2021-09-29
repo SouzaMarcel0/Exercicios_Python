8f7176306fea737149ed1e165468ca13395a0d7a
MarceloSouza <souza.marcelogi@gmail.com>
2021-09-29 15:49:47 -0300

    Create condicional.py

---
+++ Codigo_Geral/condicional.py
@@ -0,0 +1,11 @@
+
+
+resposta = input('Mamae  você comprou chocolate?[sim/não] ')
+
+
+if resposta == 'sim':
+    print('Obrigado Mainha')
+elif resposta == 'não':
+    print('Então vou chorar ... Buá, Buá')
+else:
+    print('Não foi o que eu perguntei')
