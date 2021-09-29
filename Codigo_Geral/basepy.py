d525d39b445bada5ed48355818c8be201f206d7f
MarceloSouza <souza.marcelogi@gmail.com>
2021-09-29 15:49:54 -0300

    Create basepy.py

---
+++ Codigo_Geral/basepy.py
@@ -0,0 +1,18 @@
+
+print ('*' * 24)
+print ('**** Meu Programa ****')
+print ('*' * 24)
+
+numero1 = int(input('Digite um numero inteiro: '))
+numero2 = int(input('Digite outro numero inteiro: '))
+numero3 = float(input('Digite um numero fracionado: '))
+
+resultado1 = (numero1*2) * (numero2/2)
+resultado2 = (numero1*3) + numero3
+resultado3 = numero3 ** 3
+
+
+print(f'\nA resposta da primeira pergunta é: {resultado1}\nA resposta da segunda pergunta é {resultado2}\nA resposta da terceira pergunta é: {resultado3}')
+
+
+
