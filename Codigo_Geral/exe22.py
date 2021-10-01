5c640060b930efd4113fb63f4cb7ddfeb8fa1020
MarceloSouza <souza.marcelogi@gmail.com>
2021-10-01 00:36:59 -0300

    desconto_logico

---
+++ Codigo_Geral/exe22.py
@@ -0,0 +1,24 @@
+
+
+print ('**' * 18)
+print ('**** Bem Vindo ao Meu Programa ****\n')
+
+valor = float(input('Qual o valor do produto?  '))
+
+ask_desc = input('Esse produto terá algum desconto [s/n]')
+ask_acre = input('Esse produto terá algum acresimo [s/n]')
+
+perc_desc = float(input('Digite o percentual %: '),
+
+perc_adic = float(input('Digite o percentual %: ')
+
+valor_prod_desc = valor * perc_desc / 100
+valor_prod_acres = valor + (valor * perc_adic / 100),
+valor_sem_alt = valor
+
+if ask_desc = 's'
+class ask_desc
+
+
+    
+
