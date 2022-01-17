d0c75292b7c725faeaf49db161b7d1a0f71a9418
MarceloSouza <souza.marcelogi@gmail.com>
2022-01-17 11:54:47 -0300

    Create testehacker01.py

---
+++ Codigo_Geral/testehacker01.py
@@ -0,0 +1,16 @@
+
+
+
+
+numero_1 = int(input('digite um numero  '))
+numero_2 = int(input('digite outro numero  '))
+
+print(numero_1 + numero_2)
+print(numero_1 - numero_2)
+print(numero_1 * numero_2)
+
+
+
+    
+ 
+  
